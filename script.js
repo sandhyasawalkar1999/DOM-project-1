78ffe141825dfcbdb42b53191feba3de59269352
sandhyasawalkar1999 <sandhyasawalkar1999@gmail.com>
2024-07-29 18:26:38 +0530

    dom project inilialize

---
+++ script.js
@@ -0,0 +1,29 @@
+//Use the getElementById method to retrieve the target div element.
+
+// const targetDiv = document.getElementById('target-div');
+var targetDiv = document.getElementById('target-div');
+
+console.log("targetDiv");
+//Change the background color of the fetched div element to yellow using the style.backgroundColor property. (10 Marks)
+
+// targetDiv.style.backgroundColor = 'yellow';
+targetDiv.style.backgroundColor = 'yellow';
+
+//Add a margin of 20 pixels to the element using the style.margin property.(10 Marks)
+targetDiv.style.margin ='20px';
+
+
+// Apply a padding of 10 pixels using the style.padding property.(10 Marks)
+
+targetDiv.style.padding ='10px';
+// Change the font size to 18 pixels using the style.fontSize property.(10 Marks)
+targetDiv.style.fontSize ='18px';
+// Set the font weight to bold using the style.fontWeight property. (10 Marks)
+targetDiv.style.fontWeight ='bold';
+// Change the height of the element to 200 pixels using the style.height property. (10 Marks)
+targetDiv.style.height = '200px';
+// Modify the width of the element to 300 pixels using the style.width property.(10 Marks)
+targetDiv.style.width = '300px';
+// Include a detailed README in the GitHub repository for the project.
+// In the README, provide clear instructions on how to view the hosted version of the project.
+// Include the hosted link to the live version of the project, ensuring it is accessible and functional.(10 Marks)
